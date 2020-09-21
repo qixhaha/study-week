7b173dc7501c4a8f2b3d6a267badd3d2c1fd76a5
qixuhaha <qixu17526601440@163.com>
2020-09-21 22:03:31 +0800

    vue-validate

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,20 +2,29 @@
   <div id="app">
     <div class="layui-container">
       <form class="layui-form layui-form-pane" action>
-        <div class="layui-form-item">
-          <label class="layui-form-label">密码框</label>
+        <div
+          class="layui-form-item"
+          :class="{ 'form-group--error': $v.name.$error }"
+        >
+          <label class="layui-form-label">用户名</label>
           <div class="layui-input-inline">
             <input
-              type="password"
-              name="password"
-              required
-              lay-verify="required"
-              placeholder="请输入密码"
+              type="text"
+              name="username"
+              v-model.trim="name"
+              @input="setName($event.target.value)"
+              placeholder="请输入用户名"
               autocomplete="off"
               class="layui-input"
             />
           </div>
-          <div class="layui-form-mid layui-word-aux">辅助文字</div>
+          <div class="error layui-form-mid" v-if="!$v.name.required">
+            用户名不得为空
+          </div>
+          <div class="error layui-form-mid" v-if="!$v.name.email">
+            用户名输入格式错误
+          </div>
+          <!-- <div class="layui-form-mid layui-word-aux">辅助文字</div> -->
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">密码框</label>
@@ -45,7 +54,11 @@
               class="layui-input"
             />
           </div>
-          <div class="layui-form-mid layui-word-aux" v-html="svg"></div>
+          <div
+            class="layui-form-mid layui-word-aux svg"
+            @click="getCaptcha"
+            v-html="svg"
+          ></div>
         </div>
         <button type="button" class="layui-btn">点击登录</button>
         <a href="javascript:;" class="imooc-link">忘记密码</a>
@@ -55,21 +68,51 @@
 </template>
 <script>
 import axios from 'axios';
+import { required, email } from 'vuelidate/lib/validators';
 export default {
   name: 'app',
   data() {
     return {
-      svg: ''
+      svg: '',
+      name: '',
+      password: '',
+      code: ''
     };
   },
+  validations: {
+    name: {
+      required,
+      email
+    },
+    password: {
+      required
+    },
+    code: {
+      required
+    }
+  },
   mounted() {
-    axios.get('http://localhost:3000/getCapcha').then(({ data, status }) => {
-      // console.log('验证码结果为:', status);
-      if (data.code === 200 && status === 200) {
-        this.svg = data.msg;
-      }
-    });
     // 获取验证码
+    this.getCaptcha();
+  },
+  methods: {
+    // 获取验证码
+    getCaptcha() {
+      axios.get('http://localhost:3000/getCapcha').then(({ data, status }) => {
+        // console.log('验证码结果为:', status);
+        if (data.code === 200 && status === 200) {
+          this.svg = data.msg;
+        }
+      });
+    },
+    setName(value) {
+      this.name = value;
+      this.$v.name.$touch();
+    }
+    // setAge(value) {
+    //   this.age = value;
+    //   this.$v.age.$touch();
+    // }
   }
 };
 </script>
@@ -89,5 +132,9 @@ input {
   &:hover {
     color: #009688;
   }
+}
+.svg {
+  position: relative;
+  top: -18px;
 }
 </style>
--- koa-test1/src/api/PublicController.js
+++ koa-test1/src/api/PublicController.js
@@ -1,17 +1,25 @@
 /**
  * 逻辑层
  */
-import svgCaptcha from 'svg-captcha'
+import svgCaptcha from "svg-captcha";
 class PublicController {
-    constructor() { }
-    async getCapcha(ctx) {
-        const newCaptcha = svgCaptcha.create({})
-        // console.log(newCaptcha)
-        ctx.body = {
-            code: 200,
-            msg: newCaptcha.data
-        }
-    }
+  constructor() {}
+  async getCapcha(ctx) {
+    const newCaptcha = svgCaptcha.create({
+      size: 4,
+      ignoreChars: "0o1il",
+      color: true,
+      // 干扰线
+      noise: Math.floor(Math.random() * 5),
+      width: 150,
+      height: 50,
+    });
+    // console.log(newCaptcha)
+    ctx.body = {
+      code: 200,
+      msg: newCaptcha.data,
+    };
+  }
 }
 
-export default new PublicController()
+export default new PublicController();
