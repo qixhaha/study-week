13fa25b38e63dfffdf903d90c3ee08b83021cf92
qixu <qixu17526601440@163.com>
2020-10-10 17:36:16 +0800

    数据结构栈实现四则运算

--- shujujiegou/sizeyunsuan.js
+++ shujujiegou/sizeyunsuan.js
@@ -1,1 +1,9 @@
-// 使用数组dataStore保存栈内元素
+// 使用数组dataStore保存栈内元素,构造函数将其初始化一个空数组
+// https://www.cnblogs.com/tylerdonet/p/5816464.html
+// 变量top定义栈顶的位置，构造时初始化为0，表示栈的起始位置为0
+class Stack {
+  constructor() {
+    this.dataStore = []
+    this.top = 0
+  }
+}
